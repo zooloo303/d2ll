f42cf190320886543ac6ba7f2d2891e1bec208d2
Neil Ward <zooloo@me.com>
2024-09-25 17:39:20 -0700

    update loadout actions ui

--- src/lib/components/LoadoutActions.svelte
+++ src/lib/components/LoadoutActions.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
   import { Button } from "$lib/components/ui/button";
-  import { equipLoadout, snapshotLoadout, clearLoadout } from "$lib/services/loadoutActions";
+  import {
+    equipLoadout,
+    snapshotLoadout,
+    clearLoadout,
+  } from "$lib/services/loadoutActions";
   import type { Character, Loadout } from "$lib/utils/types";
   import { Card, CardHeader, CardContent } from "$lib/components/ui/card";
   import LoadoutIdentifierDrawer from "./LoadoutIdentifierDrawer.svelte";
+  import { Axe, Camera, IdCard, CircleX } from "lucide-svelte";
 
   export let loadout: Loadout;
   export let loadoutIndex: number;
@@ -44,16 +49,22 @@
 <Card>
   <CardHeader><h3 class="text-lg font-semibold">Actions</h3></CardHeader>
   <CardContent>
-    <Button variant="ghost" on:click={handleEquipLoadout}>Equip Loadout</Button>
-    <Button variant="ghost" on:click={handleSnapshotLoadout}>Snapshot Loadout</Button>
-    <Button variant="ghost" on:click={() => identifierDrawerOpen = true}>Update Identifiers</Button>
-    <Button variant="ghost" on:click={handleEClearLoadout}>Clear Loadout</Button>
+    <Button variant="ghost" on:click={handleEquipLoadout}>Equip <Axe /></Button>
+    <Button variant="ghost" on:click={handleSnapshotLoadout}
+      >Overwrite <Camera /></Button
+    >
+    <Button variant="ghost" on:click={() => (identifierDrawerOpen = true)}
+      >Update Identifiers <IdCard /></Button
+    >
+    <Button variant="ghost" on:click={handleEClearLoadout}
+      >Clear <CircleX /></Button
+    >
     <!-- Add other action buttons here -->
   </CardContent>
-  <LoadoutIdentifierDrawer 
-    bind:open={identifierDrawerOpen} 
-    {loadout} 
-    {loadoutIndex} 
-    {character} 
+  <LoadoutIdentifierDrawer
+    bind:open={identifierDrawerOpen}
+    {loadout}
+    {loadoutIndex}
+    {character}
   />
 </Card>
